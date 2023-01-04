9fba5b2015cb9edb38c34297613b00d7c2bdb81c
mgcioce <mgcioce@gmail.com>
2023-01-04 12:32:57 -0500

    adding another example for the third use case

--- src/main/java/org/me/javastudy/anonymousclasses/MethodReferences.java
+++ src/main/java/org/me/javastudy/anonymousclasses/MethodReferences.java
@@ -14,6 +14,10 @@ public class MethodReferences {
         public String combine(String a, String b);
     }
 
+    private interface StringStuffAgain {
+        public char[] getArray(String a);
+    }
+
     private class Maths {
 
         public Integer addition(Integer a, Integer b){
@@ -51,6 +55,12 @@ public class MethodReferences {
         System.out.println("3) Reference to an instance method of an arbitrary object of a particular type (i.e. if the arguments are of the same type specify the type and the method you want to use");
         StringStuff stringCombine = String::concat;
         System.out.println(stringCombine.combine("method"," reference"));
+
+        System.out.println("Another example with one argument instead of two");
+        StringStuffAgain stringToArray = String::toCharArray;
+        char[] charArray = stringToArray.getArray("method reference");
+        for(int i = 0; i < charArray.length; i++)
+            System.out.println(charArray[i]);
 
         //4) Reference to a constructor
         System.out.println("4) Reference to a constructor method");
